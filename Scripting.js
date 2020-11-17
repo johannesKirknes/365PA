6995b4eaf27098820a5fa00095e5f59075b83473
johannesKirknes <32643474+johannesKirknes@users.noreply.github.com>
2020-11-17 15:37:00 +0100

    Update Scripting.js
    welp ill try... agian

--- Scripting.js
+++ Scripting.js
@@ -6,11 +6,20 @@ function myTimer() {
 }
 
 var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-	var radius = canvas.height / 2;
-	ctx.translate(radius, radius);
-	radius = radius * 0.90
-	setInterval(drawClock, 1000);
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+setInterval(drawClock, 1000);
+
+
+function drawClock() {
+  ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
+  ctx.fillStyle = "white";
+  ctx.fill();
+}
+
+/*
 
 function drawClock() {
   drawFace(ctx, radius);
@@ -84,3 +93,5 @@ function drawHand(ctx, pos, length, width) {
     ctx.stroke();
     ctx.rotate(-pos);
 }
+
+*\
