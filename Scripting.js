9bad1db21346ce848a065afc4c518ee3907b3d46
johannesKirknes <32643474+johannesKirknes@users.noreply.github.com>
2020-11-17 15:23:01 +0100

    Update Scripting.js
    trouble shooting fixing the javascript

--- Scripting.js
+++ Scripting.js
@@ -5,14 +5,6 @@ function myTimer() {
   document.getElementById("CurrentTime").innerHTML = d.toLocaleTimeString();
 }
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var radius = canvas.height / 2;
-ctx.translate(radius, radius);
-radius = radius * 0.90
-//drawClock();
-setInterval(drawClock, 1000);
-
 function drawClock() {
   drawFace(ctx, radius);
   drawNumbers(ctx, radius);
@@ -86,3 +78,11 @@ function drawHand(ctx, pos, length, width) {
   ctx.stroke();
   ctx.rotate(-pos);
 }
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+//drawClock();
+setInterval(drawClock, 1000);
